2e458c06693af6a17b41316d9067febc54d25901
Katy McCann <katy.alice.mccann@gmail.com>
2017-03-23 09:04:08 +0000

    Create links

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   root 'demo#index'
   get 'demo/hello'
   get 'demo/index'
+  get 'demo/other_hello'
+  get 'demo/lynda'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
